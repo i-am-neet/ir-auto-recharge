09ee8d35197df32cc31b7966c2aa6bd0000b4a27
iclab <iclab@tku.com>
2019-11-21 23:46:16 +0800

    Add conditoin: Stop the program when receiving all ir sensor

--- ir_control/script/ir_control.py
+++ ir_control/script/ir_control.py
@@ -19,6 +19,11 @@ def Rotate(x, y, theta):
 def ir_callback(d):
   pub = rospy.Publisher('motion/cmd_vel', Twist, queue_size=1)
   msg = Twist()
+  if (d.data == "ALL"):
+    x  = 0
+    y  = 0
+    z  = 0
+    rospy.signal_shutdown('Quit')
   if (d.data == "CENTER"):
     x  = BACK_SPEED
     y  = 0
