2d95cefa711f2bdf4df21daa2ad8d69bf864615b
NEET <zxc455233@gmail.com>
2019-11-27 00:07:39 +0800

    Beautify and Remove Serial

--- arduino/irrecv/irrecv.ino
+++ arduino/irrecv/irrecv.ino
@@ -9,6 +9,7 @@ decode_results results;  // 儲存訊號的結構
 byte trig = 13,
      echo = 12;
 UltraSonicDistanceSensor distanceSensor(trig, echo);
+float Charging = 0;
 
 byte RECV_CODE;
 int left, center, right;
@@ -27,7 +28,7 @@ void setup()
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
-  Serial.begin(115200);
+  //Serial.begin(115200);
   irrecv.blink13(false); // 設為true的話，當收到訊號時，腳位13的LED便會閃爍
   irrecv.enableIRIn();   // 啟動接收
 }
@@ -35,26 +36,22 @@ void setup()
 void loop()
 {
   bool stop = false;
-  float Charging = analogRead(A0);
+  Charging = analogRead(A0);
   Charging = Charging / 1024 * 25;
 
   float distance = distanceSensor.measureDistanceCm();
 
   RECV_CODE = 0b000;
-  if (irrecv.decode(&results))
-  { // 接收紅外線訊號並解碼
-    if (results.value == 0xa30)
-    {
+  if (irrecv.decode(&results)) { // 接收紅外線訊號並解碼
+    if (results.value == 0xa30) {
       left = HIGH;
       LAST_TIME_LEFT = millis();
     }
-    if (results.value == 0xa60)
-    {
+    if (results.value == 0xa60) {
       center = HIGH;
       LAST_TIME_CENTER = millis();
     }
-    if (results.value == 0xa90)
-    {
+    if (results.value == 0xa90) {
       right = HIGH;
       LAST_TIME_RIGHT = millis();
     }
@@ -77,60 +74,41 @@ void loop()
   digitalWrite(5, left);
   digitalWrite(6, center);
   digitalWrite(7, right);
-  if (Charging > 0)
-  {
+  if (Charging > 0) {
     str_msg.data = "STOP";
     chatter.publish(&str_msg);
-  }
-  else if (distance < 7 && distance > 0)
-  {
-    if (center && left && right)
-    {
+  }else if (distance < 7 && distance > 0) {
+    if (center && left && right) {
       str_msg.data = "STOP";
-    }
-    else
-    {
+    }else {
       str_msg.data = "BACK";
     }
     chatter.publish(&str_msg);
-  }
-  else if (center)
-  {
-    if (left == HIGH and right == HIGH)
-    {
+  }else if (center) {
+    if (left == HIGH and right == HIGH) {
       // All Light
       str_msg.data = "ALL";
       chatter.publish(&str_msg);
-    }
-    else if (left == HIGH and right == LOW)
-    {
+    }else if (left == HIGH and right == LOW) {
       // Left + Center
       str_msg.data = "CENTER_LEFT";
       chatter.publish(&str_msg);
-    }
-    else if (left == LOW and right == HIGH)
-    {
+    }else if (left == LOW and right == HIGH) {
       // Right + Center
       str_msg.data = "CENTER_RIGHT";
       chatter.publish(&str_msg);
-    }
-    else
-    {
+    }else {
       // Center
       str_msg.data = "CENTER";
       chatter.publish(&str_msg);
     }
-  }
-  else
-  {
-    if (left)
-    {
+  }else {
+    if (left) {
       // Left
       str_msg.data = "LEFT";
       chatter.publish(&str_msg);
     }
-    if (right)
-    {
+    if (right) {
       // Right
       str_msg.data = "RIGHT";
       chatter.publish(&str_msg);
